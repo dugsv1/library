1d0ea803fb6981f777cb6869257e28064f87f234
Sven Dugojevic <sdugoje@gmail.com>
2025-01-31 17:45:29 -0800

    Update: Update myLibrary Array on changes

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 const myLibrary = [];
 function Book(name){
     this.name = name;
+    this.read = true;
 }
 
 function addBookToLibrary(book){
     myLibrary.push(book)
 }
+function removeBookfromLibrary(bookTextName){
+    const bookIndex = myLibrary.findIndex(e => e.name === bookTextName)
+    if (bookIndex !== -1) {
+        myLibrary.splice(bookIndex,1);
+    }
+    }
 
 function updateList(book){
     let li = document.createElement("li");
@@ -51,10 +58,18 @@ submit.addEventListener('click', function (e) {
 
 ul.addEventListener('click', function (e) {
     console.log(e.target)
+    let closest_li = e.target.closest('li');
+    let bookTextName = closest_li.querySelector('span').innerText
     if (e.target.classList.contains('delete-button')) {
-        let closest_li = e.target.closest('li');
-        if (closest_li){ closest_li.remove();}
+        removeBookfromLibrary(bookTextName);
+        if (closest_li)
+            { closest_li.remove();}
     } else if (e.target.classList.contains('history')) {
+        
+        let book = myLibrary.find(e => e.name === bookTextName)
+        if (book) {
+            book.read = !book.read
+        }
         e.target.classList.toggle('not-read')
     }
 
